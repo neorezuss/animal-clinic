ce1febacd38545ae13ca56214770306db2347b1d
neorezuss <av.bysha@gmail.com>
2022-04-23 12:40:05 +0300

    initial commit

---
+++ animal-clinic-back/src/main/java/com/example/animal/clinic/back/AnimalClinicBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.animal.clinic.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalClinicBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimalClinicBackApplication.class, args);
+    }
+
+}
