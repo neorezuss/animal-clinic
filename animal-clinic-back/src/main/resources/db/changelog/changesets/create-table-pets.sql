b52e26e4aa21e5a85d16f9a998679c1de841b2a5
neorezuss <av.bysha@gmail.com>
2022-05-18 16:26:46 +0300

    database schema changes

--- animal-clinic-back/src/main/resources/db/changelog/changesets/create-table-pets.sql
+++ animal-clinic-back/src/main/resources/db/changelog/changesets/create-table-pets.sql
@@ -4,8 +4,8 @@ CREATE TABLE pets
 (
     id          BIGSERIAL PRIMARY KEY,
     name        VARCHAR NOT NULL,
-    user_id     BIGINT  NOT NULL,
-    pet_type_id BIGINT  NOT NULL,
+    user_id     BIGINT  NOT NULL REFERENCES users,
+    pet_type_id BIGINT  NOT NULL REFERENCES pets,
     breed       VARCHAR NOT NULL,
     birth_date  DATE    NOT NULL
 );
